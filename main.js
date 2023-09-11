d97eca21d929521d11000a6d6c41e35da7cf3928
Bekzod <bekzodm225@gmail.com>
2023-09-11 15:14:57 +0500

    finnshed

---
+++ main.js
@@ -0,0 +1,188 @@
+// 1.map()
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// const mapped = arr.map(element => element + 10)
+// console.log(mapped);
+
+
+// 2.filtr()
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// const filtred = arr.filter(element => element === 2 || element === 4);
+// console.log(filtred); j
+
+
+// 3.sort()
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// const alphabet = ["f", "a", "c", "v", "z"];
+//sort in descending order
+// descend = arr.sort((a, b) => a > b ? -1 : 1);
+// console.log(descend);
+//sort in ascending order
+// ascend = alphabet.sort((a, b) => a > b ? 1 : -1);
+// console.log(ascend);
+
+
+//4.forEach()
+// const arr = [1, 2, 3,];
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+
+//5.concat()
+
+// const arr1 =["a","b","c"]  
+// const arr2 =["d","e","f"]   
+
+// console.log(arr1.concat(arr2));
+// console.log(arr1);
+// console.log(arr2);
+
+
+//6.ever()
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+//all elements are agreater than 5
+// const greaterFive = arr.every(num => num > 5);
+// console.log(greaterFive);
+
+//all elements are less than 9
+
+// const lessnine = arr.every(num => num < 9);
+// console.log(lessnine);
+
+//7.some()
+
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+
+// at leats one elements is greater than 5?
+// const greaterNum = arr.some(num => num > 5);
+// console.log(greaterNum);
+
+// at leats one elements is greater than 5?
+// const lessNum = arr.some(num => num <= 0);
+// console.log(lessNum);
+
+
+//8. includes()
+
+// const arr =[1,2,3,4,5,6];
+// console.log(arr.includes(2));
+// console.log(arr.includes(7));
+
+
+//9. join()
+
+// const arr =["m","a","n","d","e","e","p"];
+// console.log(arr.join(''));
+
+//10. reduce()
+
+// const arr = [1, 2, 3, 4, 5, 6];
+// const reduced = arr.reduce((total, current) => total + current);
+// console.log(reduced);
+
+
+//11. find()
+
+// const arr = [1, 2, 4, 4, 5, 6, 7, 8, 9, 10];
+// const found = arr.find(element => element > 5)
+// console.log(found);
+
+
+//12. findIndex()
+
+// const arr = ["danny","mandeep","John","Ruby"];
+// const findFineder=arr.findIndex(element => element ==="Ruby")
+// console.log(findFineder);
+
+//13. indexOf()
+
+// const arr = ["danny","mandeep","John","Ruby"];
+// const findFineder=arr.indexOf(element => element ==="John")
+// console.log(findFineder);
+
+
+//14. fill()
+
+// const arr = new Array(3);
+// console.log(arr);
+// console.log(arr.fill(10));
+
+//15. slice()
+
+// const arr = ["a", "b", "c", "d", "e", "f"];
+// const sliced = arr.slice(1, 4)
+// console.log(sliced);
+// console.log(arr);
+
+//16. reverse()
+
+// const arr =[1,2,3];
+// arr.reverse();
+// console.log(arr);
+
+
+//17. push()
+
+
+// const fruits=["Apple","Peach"];
+// console.log(fruits.push("Banana"));
+// console.log(fruits);
+
+//18. pop()
+// const fruits=["Apple","Peach"];
+// fruits.pop();
+// console.log(fruits);
+
+
+//19. shift()
+
+// const fruits=["Apple","Peach"];
+// fruits.shift();
+// console.log(fruits);
+
+//20. unshift()
+
+// const fruits=["Apple","Peach"];
+// console.log(fruits.unshift("Banana"));
+// console.log(fruits);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
